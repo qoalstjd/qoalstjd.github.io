eaf014e383a8743ca6290ec24e2b12c5781b8639
qoalstjd <bms980805@gmail.com>
2026-01-29 15:59:16 +0900

    style: :art: 홈화면 스타일 적용

--- stayandante/pages/01_home/M_STAY_01.js
+++ stayandante/pages/01_home/M_STAY_01.js
@@ -4,7 +4,14 @@ export function init({ root }) {
         spaceBetween: 0,
         loop: true,
         effect: 'fade', // 'fade' | 'cube' | 'coverflow' | 'flip' | 'cards' | 'creative'
-
+        speed: 1000,
+        autoplay: {
+            delay: 8000,
+            disableOnInteraction: true,
+            pauseOnMouseEnter: false,
+            stopOnLastSlide: false,
+            waitForTransition: true,
+        },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
