cd91209bbf0463af1ed0b641851b267537ee46c6
qoalstjd <bms980805@gmail.com>
2026-02-10 16:50:28 +0900

    style: :art: 객실 이미지 추가 및 스타일 업데이트

--- stayandante/pages/01_home/M_STAY_01.js
+++ stayandante/pages/01_home/M_STAY_01.js
@@ -14,7 +14,7 @@ window.initModule = ({ root, params }) => {
         wrap: root.querySelector(".home-news .cont"),
         data: [],
         async init() {
-            const res = (await window.fetchManager.get("/api/news_list.php")) || [];
+            const res = (await window.fetchManager.get("https://stayandante.com/api/news_list.php")) || [];
             this.data = res.slice(0, 4);
             this.wrap.innerHTML = "";
             this.render();
@@ -28,24 +28,38 @@ window.initModule = ({ root, params }) => {
                         <div>
                             <img src="${item.thumb || "/images/home/hero_main_03.jpg"}" alt="">
                             <div class="title pd-20">
-                                <span class="tag">${item.category || "공지"}</span>
-                                <h3 class="mt-4 mb-0">${item.title}</h3>
+                                <p class="category">
+                                    <svg class="wh-20">
+                                        <use href="#ui-${item.tag === "새소식" ? "bell" : "document"}"></use>
+                                    </svg>
+                                    ${item.tag || "새소식"}
+                                </p>
+                                <h3 class="mt-4 mb-0 ell-1">${item.title}</h3>
                             </div>
                         </div>
                         <div class="flx col pd-20">
-                            <p>${item.content}</p>
+                            <p class="ell-2">${item.content}</p>
                             <p class="fs-14 mt-12">${item.created_at.split(" ")[0].split("-").join(".")}</p>
                         </div>
                     `;
                 } else {
-                    el.classList.add("box", "pc33", "bg-100");
+                    if (i === 1) {
+                        el.classList.add("box", "pc33", "bg-100");
+                    } else {
+                        el.classList.add("box", "pc25", "bg-100");
+                    }
                     el.innerHTML = `
                         <div>
-                            <span class="tag css">소식</span>
-                            <h3 class="mt-8">${item.title}</h3>
+                            <p class="category">
+                                <svg class="wh-20">
+                                    <use href="#ui-${item.tag === "새소식" ? "bell" : "document"}"></use>
+                                </svg>
+                                ${item.tag || "새소식"}
+                            </p>
+                            <h3 class="mt-8 ell-1">${item.title}</h3>
                         </div>
                         <div class="mt-auto">
-                            <p>${item.content}</p>
+                            <p class="ell-2">${item.content}</p>
                             <p class="fs-14 txt-700 mt-12">${item.created_at.split(" ")[0].split("-").join(".")}</p>
                         </div>
                     `;
@@ -53,21 +67,27 @@ window.initModule = ({ root, params }) => {
                 el.addEventListener("click", async () => {
                     window.dialog.open({
                         url: "/pages/00_common/P_STAY_00_news.html",
-                        data: await fetch(`/api/news_view.php?id=${item.id}`).then((res) => res.json()),
+                        data: await fetch(`https://stayandante.com/api/news_view.php?id=${item.id}`).then((res) => res.json()),
                     });
                 });
                 this.wrap.append(el);
             });
             this.wrap.insertAdjacentHTML(
                 "beforeend",
-                `<div class="box line pc33 jc-e ai-e">
+                `<div class="box line pc50 jc-e ai-e">
                     <div class="tit-wrap dep-3 ta-r ai-e">
                         <h4 class="ta-r">머무는 시간의 이야기를 전해요<br></h4>
                         <p>스테이 안단테의 하루, 소식으로 만나보세요</p>
                     </div>
-                    <div class="btn-wrap jc-e mt-auto">
-                        <a href="/index.html?linkcd=m0500" class="btn md line mt-auto">
-                            안단테 소식 보기
+                    <div class="btn-wrap jc-e mt-0">
+                        <a href="/index.html?linkcd=m0501" class="btn md line">
+                            안단테 소식
+                            <svg class="wh-20">
+                                <use href="#dir-chevron-right"></use>
+                            </svg>
+                        </a>
+                        <a href="/index.html?linkcd=m0502" class="btn md line">
+                            FAQ
                             <svg class="wh-20">
                                 <use href="#dir-chevron-right"></use>
                             </svg>
--- stayandante/pages/05_cs/M_STAY_05_01.js
+++ stayandante/pages/05_cs/M_STAY_05_01.js
@@ -4,8 +4,7 @@ window.initModule = ({ root, params }) => {
         wrap: root.querySelector(".meta-list"),
         data: [],
         async init() {
-            const res = (await window.fetchManager.get("/api/news_list.php")) || [];
-            this.data = res.slice(0, 4);
+            this.data = (await window.fetchManager.get("https://stayandante.com/api/news_list.php")) || [];
             this.wrap.innerHTML = "";
             this.render();
         },
@@ -14,13 +13,19 @@ window.initModule = ({ root, params }) => {
                 const el = document.createElement("li");
                 el.innerHTML = `
                     <span class="id">${item.id}</span>
-                    <span class="tag">item.tag</span>
+                    <span class="category">
+                        <svg class="wh-20">
+                            <use href="#ui-${item.tag === "새소식" ? "bell" : "document"}"></use>
+                        </svg>
+                        ${item.tag || "새소식"}
+                    </span>
                     <span class="title">${item.title}</span>
-                    <span class="date">${item.created_at}</span>`;
+                    <span class="date">${item.created_at.split(" ")[0].split("-").join(".")}</span>
+                    <span class="view">${item.view_cnt}</span>`;
                 el.addEventListener("click", async () => {
                     window.dialog.open({
                         url: "/pages/00_common/P_STAY_00_news.html",
-                        data: await fetch(`/api/news_view.php?id=${item.id}`).then((res) => res.json()),
+                        data: await fetch(`https://stayandante.com/api/news_view.php?id=${item.id}`).then((res) => res.json()),
                     });
                 });
                 this.wrap.append(el);
