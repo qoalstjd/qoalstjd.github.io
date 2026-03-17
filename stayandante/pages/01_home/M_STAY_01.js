c09831c12c9a9232bc05cafaee17add478bc9c38
qoalstjd <bms980805@gmail.com>
2026-03-18 08:55:10 +0900

    style: :art: 스타일 업데이트

--- stayandante/pages/01_home/M_STAY_01.js
+++ stayandante/pages/01_home/M_STAY_01.js
@@ -9,6 +9,88 @@ window.initModule = ({ root, params }) => {
         pagination: { el: swiperEl.querySelector(".swiper-pagination"), clickable: true },
         speed: 1500,
     });
+    // 주변소개
+    const around = {
+        wrap: root.querySelector(".home-arround .cont"),
+        list: [
+            "127801", // 석모도
+            "128818", // 민머루 해수욕장
+            "125533", // 보문사
+            "2490906", // 자연휴양림
+            "2490886", // 석모도 미네랄온천
+            "2758257", // 외포항
+            "3340348", // 강화함상공원
+        ],
+        data: [],
+        async init() {
+            try {
+                this.data = (await window.fetchManager.get(`https://qoalstjdapis.vercel.app/api/getTour?contentids=${this.list.join(",")}`)) || [];
+            } catch (e) {
+                console.error("관광정보목록 조회 실패", e);
+                this.data = [];
+            }
+            this.render();
+        },
+        infoFilter(value) {
+            return value || "-";
+        },
+        render() {
+            this.data.slice(0, 4).forEach((item, i) => {
+                const el = document.createElement("div");
+                if (i === 0) {
+                    el.classList.add("box", "pc66", "pd-0");
+                    el.innerHTML = `
+                        <img src="${item.info.thumbnail || item.images[0]}" alt="${item.title}">
+                        <div class="pd-24 bg-700 txt-100 mt-auto" style="position:relative;">
+                            <h3 class="mb-8">${item.title}</h3>
+                            <p class="ell-3 fs-14">${item.summary}</p>
+                        </div>
+                    `;
+                } else {
+                    if (i === 1) {
+                        el.classList.add("box", "pc33");
+                    } else {
+                        el.classList.add("box", "pc25");
+                    }
+                    el.innerHTML = `
+                        <img src="${item.info.thumbnail || item.images[0]}" alt="${item.title}">
+                        <div class="box" style="height:100%;">
+                            <h3 class="mt-0 mb-8">${item.title}</h3>
+                            <p class="ell-3 mb-12 fs-14">${item.summary}</p>
+                            <ul class="txt-list dot mt-auto fs-14 txt-700">
+                                <li>주소 : ${this.infoFilter(item.info.address)}</li>
+                                <li>전화번호 : ${this.infoFilter(item.info.infocenter)}</li>
+                                <li>휴일 : ${this.infoFilter(item.info.restdate)}</li>
+                                <li>이용시간 : ${this.infoFilter(item.info.usetime)}</li>
+                                <li>주차 : ${this.infoFilter(item.info.parking)}</li>
+                            </ul>
+                        </div>
+                    `;
+                }
+                this.wrap.append(el);
+            });
+            this.wrap.insertAdjacentHTML(
+                "beforeend",
+                `<div class="box pc50">
+                    <img src="${this.data[0].images[0] || "/images/home/hero_main_01.jpg"}" alt="" />
+                    <div class="box jc-e ai-e">
+                        <div class="tit-wrap dep-3 ta-r ai-e">
+                            <h3 class="ta-r">주변 관광 안내</h3>
+                            <p>스테이안단테펜션에서 만나는 여행 이야기</p>
+                        </div>
+                        <div class="btn-wrap jc-e mt-0">
+                            <a href="/index.html?linkcd=m0501" class="btn md line">
+                                주변 여행 더 알아보기
+                                <svg class="wh-20">
+                                    <use href="#dir-chevron-right"></use>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>`
+            );
+        },
+    };
     // 새소식
     const news = {
         wrap: root.querySelector(".home-news .cont"),
@@ -126,5 +208,7 @@ window.initModule = ({ root, params }) => {
             }
         },
     };
+
+    around.init();
     // news.init();
 };
