975146937d939a3a53dc85f6a4e8682938fb4deb
qoalstjd <bms980805@gmail.com>
2026-04-27 10:55:49 +0900

    이미지업데이트

--- stayandante/pages/02_about/M_STAY_02_02.js
+++ stayandante/pages/02_about/M_STAY_02_02.js
@@ -1,2 +1,16 @@
 window.initModule = ({ root, params }) => {
+    const { linkcd } = params;
+    const detailImg = () => {
+        const imgEls = root.querySelectorAll(".cont .img-wrap img");
+        imgEls.forEach((img) => {
+            const mainImgEl = img.closest(".visual").querySelector(":scope > img");
+            const siblings = img.closest(".img-wrap").querySelectorAll("img");
+            img.addEventListener("click", () => {
+                siblings.forEach((el) => el.classList.remove("is-active"));
+                img.classList.add("is-active");
+                mainImgEl.src = img.src;
+            });
+        });
+    };
+    detailImg();
 };
